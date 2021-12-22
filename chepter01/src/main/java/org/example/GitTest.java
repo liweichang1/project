82e3afc7b91e7429a78f7a5c7889984b576f2590
liweichang1 <80303348+liweichang1@users.noreply.github.com>
2021-12-22 17:22:24 +0800

    pull test

--- chepter01/src/main/java/org/example/GitTest.java
+++ chepter01/src/main/java/org/example/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hot-fix test");
         System.out.println("push commit!!!");
         System.out.println("push commit222");
+        //pull test
     }
 }
