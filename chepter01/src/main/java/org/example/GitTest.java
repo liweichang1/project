f288701e6d6ac383df79243fb66db01ac12fe4d9
liweichang <li@163.com>
2021-12-22 16:37:02 +0800

    master commit

--- chepter01/src/main/java/org/example/GitTest.java
+++ chepter01/src/main/java/org/example/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("你好");
         System.out.println("第三次提交");
         System.out.println("hot-fix");
+        System.out.println("master test");
     }
 }
